1cd7e70238d0eaa3f49bfe650fbc22c623a34b93
ak55m <60115485+ak55m@users.noreply.github.com>
2020-08-13 23:55:55 -0500

    Update index.php

--- ecom2/public/index.php
+++ ecom2/public/index.php
@@ -9,18 +9,10 @@
 
 
   <!-- Page Content -->
-  <div class="container-fluid">
-
-       
-      
-    <div class="row">
-<!--        <div class="col-sm-1"></div>-->
-      <!-- Blog Entries Column -->
-<div style="margin-left: 100px; margin-right: 700px;" > 
-        
-<!--      <div class="col-lg-8">-->
-            
-            <br>
+  <div class="container-fluid"> 
+   <div class="row">
+   <div style="margin-left: 100px; margin-right: 700px;" > 
+   <br>
 
         <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
           <ol class="carousel-indicators">
@@ -47,43 +39,14 @@
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
           </a>
-        </div>
-            
-           
-            
+         </div>
               <div class="row">
-                  
-<!--                 <?php echo $_SESSION['product_1']; ?>-->
-                  
                   <br>
                   <br>
-                  
             <?php get_products(); ?>   
-
-            
-          
-
             </div>
-                  
-        <!-- Pagination -->
-<!--
-        <ul class="pagination justify-content-center mb-4">
-          <li class="page-item">
-            <a class="page-link" href="#">&larr; Older</a>
-          </li>
-          <li class="page-item disabled">
-            <a class="page-link" href="#">Newer &rarr;</a>
-          </li>
-        </ul>
--->
       </div>
-
       </div>
-        
-
-   
-        
-        
  <?php include(TEMPLATE_FRONT . DS . "side_widget.php")?>
         
         
